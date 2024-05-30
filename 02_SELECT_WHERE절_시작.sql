c08b411a3738bad1fa7c5dd208c5e951fb05a8c8
Wonki11 <wongi5011@daum.net>
2024-05-30 12:45:21 +0900

    select 정리

--- 02_SELECT_WHERE절_시작.sql
+++ 02_SELECT_WHERE절_시작.sql
@@ -1,3 +1,28 @@
+/*
+SELECT WHERE 절
+WHERE 의 경우 조건을 걸어 선택한 조건에 해당하는 행만 볼 수 있도록 설정
+WHERE 원하는컬럼 = 원하는컬럼에서 해당하는 조건;
+
+만약 아무것도 없는 NULL 값을 조회하길 원한다면
+WHERE 원하는 컬럼 IS NULL;
+
+만약 NULL 이 아닌 값만 조회하길 원한다면
+WHERE 원하는컬럼 IS NOT NULL;
+
+~이상이거나 ~이하일 때 >= <= > < 사용 (미만,초과 포함)
+WHERE 원하는컬럼 >= 원하는값;
+
+00과 00사이를 조회하길 원한다면 BETWEEN 00 AND 00;
+에 WHERE 키(KEY) BETWEEN 사당 AND 의정부;
+
+-- WHERE 절 사용 X
+ORDER BY
+오름차순 ASC
+내림차순 DESC
+
+*/
+
+
 -- 모든 직원 정보 조회
 SELECT * FROM employee;
 
@@ -60,5 +85,53 @@ WHERE DEPT_CODE IS NULL;
 --DEPT_CODE 가 모두 IS NULL 인 사람의 모든 정보를 조회
 SELECT * FROM EMPLOYEE WHERE DEPT_CODE IS NULL;
 
+--보너스가 없는 사원의 이름과 보너스를 조회
+SELECT EMP_NAME,BONUS FROM EMPLOYEE WHERE BONUS IS NULL;
 
+--급여 등급이 S5인 사원의 이름과 급여 조회 SAL_LEVEL
+SELECT EMP_NAME,SALARY FROM EMPLOYEE WHERE SAL_LEVEL = 'S5';
+
+--사원 번호가 210인 사원의 모든 정보 조회 EMP_ID
+SELECT * FROM EMPLOYEE WHERE EMP_ID = '210';
+
+--이메일 주소가 NULL 이 아닌 사원의 이름과 이메일 조회
+SELECT EMP_NAME,EMAIL FROM EMPLOYEE WHERE EMAIL IS NOT NULL;
+
+--보너스가 0.2 이상인 사원의 이름과 보너스를 조회
+SELECT EMP_NAME,BONUS FROM EMPLOYEE WHERE BONUS >= 0.2;
+
+--급여가 3000000 에서 4000000 사이인 사원의 이름과 급여 조회
+SELECT EMP_NAME,SALARY FROM EMPLOYEE WHERE SALARY BETWEEN 3000000 AND 4000000;
+
+--급여가 높은 순서대로 사원의 이름과 급여 조회
+--숫자체계 오름차순 1부터 시작
+--숫자체계 내림차순 9부터 시작
+SELECT EMP_NAME,SALARY FROM EMPLOYEE ORDER BY SALARY DESC;
+
+--사원의 이름을 오름차순으로 정렬해서 조회
+--글자체계 오름차순 ㄱ 부터 ㅎ까지 작성하기 A-Z
+--글자체계 내림차순 G  부터 ㄱ까지 작성하기 Z-A
+SELECT EMP_NAME FROM EMPLOYEE ORDER BY EMP_NAME ASC;
+
+--EMPLOYEE 테이블에서 급여가 300만원을 초과하는 사원의 사번 이름 급여 부서코드 조회
+SELECT EMP_NAME,EMP_ID,SALARY,JOB_CODE FROM EMPLOYEE WHERE SALARY > 3000000;
+
+--EMPLOYEE 테이블에서 연봉이 5천만원 이하인 사원의 사번 이름 연봉 조회 연봉은 연봉으로 표기
+SELECT EMP_NAME,EMP_ID,SALARY * 12 AS "연봉" FROM EMPLOYEE WHERE SALARY * 12 <= 50000000;
+
+--EMPLOYEE 테이블에서 부서코드가 'D9'가 아닌 사원들의 이름 부서코드 전화번호 조회 PHONE
+SELECT EMP_NAME,DEPT_CODE,PHONE FROM EMPLOYEE WHERE DEPT_CODE != 'D9';
+
+--EMPLOYEE 테이블에서 부서코드가 'D6' 사원 중 급여가 300만원을 초과하는 사원의 이름 부서코드 급여조회
+SELECT EMP_NAME,DEPT_CODE,SALARY FROM EMPLOYEE WHERE DEPT_CODE = 'D6' AND SALARY > 3000000;
+
+--부서코드가 D6이고 직급코드가 J3인 사원의 이름과 급여 JOB_CODE
+SELECT EMP_NAME,SALARY FROM EMPLOYEE WHERE DEPT_CODE = 'D6' AND JOB_CODE = 'J3';
+
+--날짜 범위 비교 EMPLOYEE 테이블에서 입사일이 2000-01-01 부터 2000-12-31 사이인 이름,입사일 조회
+SELECT EMP_NAME,HIRE_DATE AS "입사일" FROM EMPLOYEE WHERE HIRE_DATE BETWEEN TO_DATE ('2000-01-01','YYYY-MM-DD')
+AND TO_DATE('2000-12-31','YYYY-MM-DD');
+
+--입사일이 2000년 이후 입사한 사원의 이름과 입사일 조회
+SELECT EMP_NAME,HIRE_DATE AS "입사일" FROM EMPLOYEE WHERE HIRE_DATE BETWEEN TO_DATE ('2000','YYYY') AND TO_DATE ('2999','YYYY');
 
