5554e621b974bc264a960225d3a7cef255066b20
Wonki11 <wongi5011@daum.net>
2024-06-04 12:48:37 +0900

    join 실습 코드 작성

--- SQL_코드/05_GROUPBY_실습.sql
+++ SQL_코드/05_GROUPBY_실습.sql
@@ -80,3 +80,123 @@ FROM EMPLOYEE
 GROUP BY DEPT_CODE,JOB_CODE,HIRE_DATE
 ORDER BY DEPT_CODE,JOB_CODE,HIRE_DATE;
 --공채 채용 후 기수별 남아있는 사원 수 조회할 때 사용
+
+----------------------------------------------------------------
+
+--WHERE 절 : 지정된 테이블에서 어떤 행만을 조회 결과로 삼을 것이닞 조건을 지정하는 구문
+            --(테이블 내에서 특정 컬럼만 뽑아서 사용하겠다는 조건문)
+            
+--HAVING 절 : 그룹 함수로 구해 올 그룹에 대한 조건을 설정할 때 사용
+            --(그룹에 대한 조건, 어떤 그룹만 조회하겠다)
+
+--부서별 평균 급여가 3000000원 이상인 사원만 조회하고 오름차순 정렬    
+-->모든 직원들을 대상으로 그룹화한 후 각 부서별로 평균 급여를 계산
+-- HAVING 절을 이용해서 계산된 평균 급여가 3000000원 이상인 부서만 필터링
+-- 모든 급여를 고려해서 평균을 계산한다음 조건에 맞는 결과만 필터링
+SELECT DEPT_CODE,AVG(SALARY)
+FROM EMPLOYEE
+WHERE SALARY >= 3000000
+GROUP BY DEPT_CODE
+--HAVING AVG(SALARY) >= 3000000
+ORDER BY DEPT_CODE;
+
+
+SELECT DEPT_CODE,AVG(SALARY)
+FROM EMPLOYEE
+--WHERE SALARY >= 3000000
+GROUP BY DEPT_CODE
+HAVING AVG(SALARY) >= 3000000
+ORDER BY DEPT_CODE;
+
+/*
+D1	3660000
+D5	3630000
+D6	3650000
+D9	5900000
+
+D6	3366666.66666666666666666666666666666667
+D9	5900000
+
+WHERE절의 경ㅇ 조건에 맞는 값만 가지고 온 후 보여준다면
+HAVING절의 경우 모든 값을 확인하고 조건에 부합하는 그룹만 보여줌
+*/
+
+/* HAVING 실습 */
+--1. 각 부서별 평균 급여가 400만원 이상인 부서 조회
+SELECT DEPT_CODE, AVG(SALARY) AS "평균월급"
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING AVG(SALARY) >= 4000000;
+--2. 각 부서별 직원 수가 5명 이상인 부서 조회
+SELECT DEPT_CODE, COUNT(*) AS "부서별 직원 수"
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING COUNT(*) >= 5;
+-- D5 6
+
+--3. 각 부서별 보너스 평균이 0.2이상인 부서조회
+SELECT DEPT_CODE, COUNT(*) AS "보너스 평균"
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING COUNT(*) >= 0.2;
+
+--4. 각 직급별 최소 급여가 300만 이하인 직급 조회
+--MIN AS "최소급여"
+SELECT JOB_CODE, MIN(SALARY) AS "평균급여"
+FROM EMPLOYEE
+GROUP BY JOB_CODE
+HAVING MIN(SALARY) <= 3000000;
+
+
+--5. EMPLOYEE테이블에서
+--부서별 70년대생의 급여 평균이 300만 이상인 부서를 조회하고 부서코드 오름차순
+SELECT DEPT_CODE,FLOOR(AVG(SALARY)) AS 급여평균
+FROM EMPLOYEE
+-- WHERE TO_NUMBER(SUBSTR(EMP_NO,1,2)) BETWEEN 70 AND 79
+WHERE SUBSTR(EMP_NO, 1,1) = '7' --SUBSTR 원하는 숫자 값을 출력하려면 = '숫자' 
+GROUP BY DEPT_CODE,EMP_NO
+HAVING AVG(SALARY)>= 3000000
+ORDER BY 급여평균 ASC;
+
+/* ROLLUP CUBE*/
+--1.부서별 직급별 급여 합계
+SELECT DEPT_CODE,JOB_CODE,SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY ROLLUP(DEPT_CODE,JOB_CODE)
+ORDER BY DEPT_CODE;
+
+--2. 부서별,직급별,급여 등급별 급여 합계
+SELECT DEPT_CODE,JOB_CODE,SAL_LEVEL,SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY CUBE(DEPT_CODE,JOB_CODE,SAL_LEVEL)
+ORDER BY DEPT_CODE; -- ASC 생략
+
+--3. 부서별 직급별 급여 평균 ROLLUP 오름차순
+SELECT DEPT_CODE,JOB_CODE,FLOOR(AVG(SALARY))
+FROM EMPLOYEE
+GROUP BY ROLLUP(DEPT_CODE,JOB_CODE)
+ORDER BY DEPT_CODE ASC;
+
+--4. 부서별 직급별 직원수 ROLLUP 오름차순
+SELECT DEPT_CODE,JOB_CODE,COUNT(*) AS "직원수"
+FROM EMPLOYEE
+GROUP BY ROLLUP(DEPT_CODE,JOB_CODE)
+ORDER BY DEPT_CODE ASC;
+
+--5. 부서별 직급별 급여 등급별 평균 급여 내림차순
+SELECT DEPT_CODE,JOB_CODE,SAL_LEVEL,FLOOR(AVG(SALARY))
+FROM EMPLOYEE
+GROUP BY CUBE(DEPT_CODE,JOB_CODE,SAL_LEVEL)
+ORDER BY FLOOR(AVG(SALARY)) DESC;
+
+--5. 부서별 직급별 급여 등급별 보너스 합계 내림차순
+SELECT DEPT_CODE,JOB_CODE,SAL_LEVEL,SALARY,SUM(BONUS)
+FROM EMPLOYEE
+GROUP BY CUBE(DEPT_CODE,JOB_CODE,SAL_LEVEL,SALARY)
+ORDER BY DEPT_CODE DESC;
+
+            
+            
+            
+            
+          
