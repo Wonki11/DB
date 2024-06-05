749ae148e3a3dc1ae0ca326a62e3b10dc763a8c8
Wonki11 <wongi5011@daum.net>
2024-06-05 12:47:47 +0900

    조건문 실습

---
+++ SQL_코드/08_조건문_실습.sql
@@ -0,0 +1,38 @@
+/*DECODE 실습*/
+
+-- 1) 주어진 학생의 성별 출력
+-- SELECT STUDENT_NAME,STUDENT_SSN,DECODE 성별 확인
+SELECT * FROM TB_STUDENT;
+SELECT STUDENT_NAME , DECODE(SUBSTR(STUDENT_SSN,8,1),'1','남자','2','여자')
+FROM TB_STUDENT;
+
+-- 2) D1 인사관리부 , D2 회계관리부 , D3 마케팅부 D4 국내영업부 기타 DECODE
+-- SELECT EMP_NAME, DEPT_CODE FROM EMPLOYEE
+SELECT EMP_NAME , DECODE(SUBSTR(DEPT_CODE,2,1),'1','인사관리부','2','회계관리부','3','마케팅부','4','국내영업부','기타')
+FROM EMPLOYEE;
+
+--3) SELECT EMP_NAME, JOB_CODE FROM EMPLOYEE
+SELECT EMP_NAME, DECODE(SUBSTR(JOB_CODE,2,1),'1','대표','2','부사장','3','부장','4','차장','5','과장','6','대리','7','사원')AS "직급"
+FROM EMPLOYEE;
+
+/*** CASE WHEN THEN ELSE END 실습 *****/
+-- 1) 급여에 따라 등급 설 정 600만 이상 S1 500만 이상 S2 400 S3 나머지 S4 AS 급여등급
+SELECT EMP_NAME,SALARY,
+CASE WHEN SALARY >= 6000000 THEN 'S1'
+     WHEN SALARY >= 5000000 THEN 'S2' 
+     WHEN SALARY >= 4000000 THEN 'S3'
+     ELSE 'S4'
+     END AS "급여등급" 
+FROM EMPLOYEE;
+
+-- 2) 사원의 근무 일 수 에 따라 근속기간 분류하는 CASE 
+-- SELECT EMP_NAME, HIRE_DATE,
+--FLOOR(MONTHS_BETWEEN(SYSDATE,HIRE_DATE/12)>= 20 THEN '20년 이상' 
+-- 10 THEN 10년 이상
+--10년 미만 근속기간 분류
+SELECT EMP_NAME, HIRE_DATE,
+        CASE WHEN FLOOR(MONTHS_BETWEEN(SYSDATE,HIRE_DATE)/12)>= 20 THEN '20년 이상'
+        WHEN FLOOR(MONTHS_BETWEEN(SYSDATE,HIRE_DATE)/12)>= 10 THEN '10년 이상'
+        ELSE '근속기간 분류' 
+        END AS "근속기간 분류"
+        FROM EMPLOYEE;
